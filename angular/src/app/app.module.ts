63b41bbe50f71d365f327f91cff963347308f71f
hieunguyen29 <105211474+hieunguyen29@users.noreply.github.com>
2023-05-21 20:37:19 +0700

    suacode-filesmain

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { NotFound404Component } from './pages/not-found404/not-found404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotFound404Component
   ],
   imports: [
     BrowserModule
